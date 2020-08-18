0e54291175611ce6bb96eebe0d6892d2b8c9098d
Juan_Lopera <juan.lopera.e@gmail.com>
2020-08-18 07:52:19 -0500

    retos

---
+++ primeros-programas/igual-8.js
@@ -0,0 +1,9 @@
+var num = 10;
+if (num === 8) {
+    console.log("number es igual a 8");
+} else {
+    console.log("No es 8")
+}
+var number = 9
+var message = number === 8 ? "El número es igual a 8" : "No es 8";
+console.log(message);
---
+++ primeros-programas/mayor-que-diez.js
@@ -0,0 +1,3 @@
+var num = 8
+if (num > 10);
+console.log("Es mayor que 10");
