61ae41515a42cdf06f100bc063f5f6f91753d7ee
Juan <juan.lopera.e@gmail.com>
2020-09-08 21:17:14 -0500

    ultimo ejercicio

---
+++ primeros-programas/12-arrow-functions.js
@@ -0,0 +1,12 @@
+const BMI = (weight , height) => {
+    return weight / (height*height);
+};
+console.log(BMI(86, 1.8));
+
+const cuadradoV2 = numero => {
+    return numero*numero;
+};
+console.log(cuadradoV2(3));
+
+const cuadradoV3 = numero => numero*numero;
+console.log(cuadradoV3(3));
